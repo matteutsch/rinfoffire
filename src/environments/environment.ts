58a455e9f60145fccd38d905364cd950eacec077
matteutsch <matteutsch@gmail.com>
2023-07-25 15:33:35 +0200

    updated firebase & angular/fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
+    projectId: 'ring-of-fire-35483',
+    appId: '1:921656553054:web:3f57c5e1992cb1c839cecf',
+    storageBucket: 'ring-of-fire-35483.appspot.com',
     apiKey: 'AIzaSyAZNKDcs8JPTO2Qb_VtxAF_7skSLdt81EM',
     authDomain: 'ring-of-fire-35483.firebaseapp.com',
-    projectId: 'ring-of-fire-35483',
-    storageBucket: 'ring-of-fire-35483.appspot.com',
     messagingSenderId: '921656553054',
-    appId: '1:921656553054:web:3f57c5e1992cb1c839cecf',
   },
 };
